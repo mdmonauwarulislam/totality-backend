209fd72931714a3bdd758b63789767f9e0601426
Md Monauwarul Islam <mdmonauwarulislam@example.com>
2024-09-27 22:40:13 +0530

    correction

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -7,8 +7,8 @@ const { verifyToken, verifyAdmin } = require("../middleware/authMiddleware");
 
 
 // user routes
-Router.post("/user/signup", userRegistration);
-Router.post("/user/login", userLogin);
+Router.post("/signup", userRegistration);
+Router.post("/login", userLogin);
 
 
 
